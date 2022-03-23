90f46e48722b1d34673808a41eb729b727a8efb0
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-03-23 00:59:00 -0300

    set a basic navigation with router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {BrowserRouter,Switch, Route, Link} from "react-router-dom"
+import {About} from "./pages/About"
+import {Home} from "./pages/Home"
+import {Posts} from "./pages/Posts"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <h1>Hello from the future</h1>
+        <ul>
+          <li>
+            <Link to="/"> Home</Link>
+          </li>
+          <li>
+          <Link to="/about"> About</Link>
+          </li>
+          <li>
+          <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/Posts">
+            <Posts/>
+          </Route>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 export const About = () => {
     return (
         <h1>About</h1>
