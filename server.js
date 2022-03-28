74f48139ad0124a9054bf35416dac711c84ccb17
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-03-27 23:59:49 -0300

    routing SSR and replace id="root" with reactApp

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import express from "express"
 import React from "react"
 //do the actual rendering of the app
 import {renderToString} from "react-dom/server"
-import {Home} from "./src/pages/Home"
+// this s the server side equivalent of BrowaweRouter
+import {StaticRouter} from "react-router-dom/server"
+import App from "./src/App"
+import path from "path"
+import fs from "fs"
 
 const app = express()
 
@@ -14,16 +18,22 @@ app.use(express.static('./build',{index:false}))
 app.get("/*", (req,res) => {
 // renderToString is a function that we can pass JSX to, take that and render it into actual html sting
     const reactApp = renderToString(
-        <Home />
-    )
-// send the html string back to de client
-    return res.send(`
-    <html>
-        <body>
-            <div id="root">${reactApp}</div>
-        </body>
-    </html>
-    `)
+        // in order for it to work, we have to set up routing on our server for the frontend
+        <StaticRouter location={req.url}>
+            <App />
+        </StaticRouter>    
+    );
+
+    const templateFile = path.resolve('./build/index.html');
+    fs.readFile(templateFile, 'utf8', (err,data) => {
+        if(err) {
+            return res.status(500).send(err);
+        }
+        //load the index.html file for the build folder and manually replace id=root with reactApp
+        return res.send(
+            data.replace('<div id="root"></div>',`<div id="root">${reactApp}</div>`)
+        )
+    });
 });
 
 app.listen(8080, () => {
